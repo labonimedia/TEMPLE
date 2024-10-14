6004b3fb3838f62e5f63922120308d7f5ef3c36a
bhusnarsd <bhusnarsd@gmail.com>
2024-10-14 11:42:33 +0530

    chnages

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -25,7 +25,7 @@ const uploadFile = async (file) => {
   // eslint-disable-next-line no-useless-catch
   try {
     await s3Client.send(command);
-    return `https://lmscontent-cdn.blr1.digitaloceanspaces.com/b2b/temple/${params.Key}`;
+    return `https://lmscontent-cdn.blr1.digitaloceanspaces.com/temple/${params.Key}`;
   } catch (err) {
     throw err; // Rethrow the error after logging it
   }
